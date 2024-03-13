ef0ceb8986ddfd48c1f213a0e5ce9323638438ad
fedraameneiro <130572241+fedraameneiro@users.noreply.github.com>
2024-03-13 16:31:54 -0300

    Add files via upload

--- a.py
+++ a.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import requests
-import pandas as pd
-
-Modelo, Preprocesador=load('algoritmo.joblib')
-
 
 def main():
     st.title("Formulario de Datos del Paciente")
@@ -19,42 +14,6 @@ def main():
 
     # Mostrar los datos del paciente en la interfaz de usuario
     st.write("Datos del Paciente:", paciente)
-
-    # Llamar al modelo de machine learning
-    resultado_prediccion = predecir_con_modelo(paciente)
-
-    # Mostrar el resultado de la predicción
-    st.write("Resultado de la Predicción:", resultado_prediccion)
-
-def predecir_con_modelo(paciente):
-    # URL del modelo en GitHub
-    url_modelo = "https://raw.githubusercontent.com/fedraameneiro/DataScientistFinalProject/main/Proyecto_final_Ciencia_de_Datos_FedraAmeneiro_api.ipynb"
-
-    # Realizar una solicitud HTTP para obtener el modelo
-    response = requests.get(url_modelo)
-
-    # Verificar si la solicitud fue exitosa
-    if response.status_code == 200:
-        # Cargar el modelo desde el contenido de la respuesta
-        modelo = cargar_modelo_desde_bytes(response.content)
-
-        # Realizar la predicción
-        resultado_prediccion = modelo.predict([paciente])
-
-        # Devolver el resultado de la predicción
-        return resultado_prediccion[0]
-
-    else:
-        st.error(f"No se pudo obtener el modelo. Código de estado: {response.status_code}")
-        return None
-
-def cargar_modelo_desde_bytes(modelo_bytes):
-    # Esta función debe ser implementada según la forma en que guardaste tu modelo.
-    # Puedes usar pickle, joblib, o cualquier otro formato de serialización.
-    # Aquí, como ejemplo, se asume que se utiliza pickle.
-    import pickle
-    modelo = pickle.loads(modelo_bytes)
-    return modelo
 
 if __name__ == "__main__":
     main()
