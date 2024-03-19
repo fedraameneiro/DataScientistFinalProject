a058143405df48f3146a6c6a948dbd7168edd82c
fedraameneiro <130572241+fedraameneiro@users.noreply.github.com>
2024-03-19 11:18:50 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,19 @@
-import streamlit as st
-st.title("Formulario de Datos del Paciente")
-valor_edad = st.slider("Edad", min_value=0, max_value=100, value=73)
-valor_bmi = st.text_input("BMI", "25.91")
-valorHbA1c =  st.text_input("Valor hbA1c", "9")
-valor_glucosa = st.text_input("Valor glucosa", "160")
+
+
+
+
+
+# Crear un array llamado 'paciente' con los datos recopilados
+    paciente = [valor_edad, valor_bmi, valorHbA1c, valor_glucosa]
+
+    # Mostrar los datos del paciente en la interfaz de usuario
+    st.write("Datos del Paciente:", paciente)
+    
+    paciente= np.array([[valor_edad, valor_bmi, valorHbA1c, valor_glucosa]])
+    scaler = StandardScaler()
+    paciente = scaler.fit_transform(paciente)
+    nuevos_datos_scaled = scaler.transform(paciente)
+
+    resultado_prediccion = modelo.predict(nuevos_datos_scaled)
+
   
