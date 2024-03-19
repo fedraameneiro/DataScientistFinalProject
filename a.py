8858cd4f0acb899562eb6a6d35bad05524f0ce6c
fedraameneiro <130572241+fedraameneiro@users.noreply.github.com>
2024-03-19 11:20:31 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,20 @@
+import streamlit as st
+import pandas as pd
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from keras.models import load_model 
 
-
-
-
-
-# Crear un array llamado 'paciente' con los datos recopilados
+# Cargar el modelo
+modelo = load_model('modelo.h5')
+def main():
+    st.title("Formulario de Datos del Paciente")
+    # Recopilar datos del usuario
+    valor_edad = st.slider("Edad", min_value=0, max_value=100, value=73)
+    valor_bmi = st.text_input("BMI", "25.91")
+    valorHbA1c =  st.text_input("Valor hbA1c", "9")
+    valor_glucosa = st.text_input("Valor glucosa", "160")
+    
+    # Crear un array llamado 'paciente' con los datos recopilados
     paciente = [valor_edad, valor_bmi, valorHbA1c, valor_glucosa]
 
     # Mostrar los datos del paciente en la interfaz de usuario
@@ -15,5 +26,8 @@
     nuevos_datos_scaled = scaler.transform(paciente)
 
     resultado_prediccion = modelo.predict(nuevos_datos_scaled)
+    
+
+main()
 
   
